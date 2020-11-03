80c35383591e66baf2a4433d6a572a4b95613a19
manh.dd <manh.dd@citigo.com.vn>
2020-11-03 14:28:43 +0700

    update

--- resources/js/repositories/clients/axiosClient.js
+++ resources/js/repositories/clients/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseDomain = "https://sale.bimsuasosinh.com/api/";
+const baseDomain = "http://127.0.0.1:8000/api/";
 const baseURL = `${baseDomain}`; // Incase of /api/v1;
 
 // ALL DEFUALT CONFIGURATION HERE
